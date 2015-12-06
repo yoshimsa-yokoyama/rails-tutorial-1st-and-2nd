1affcd213dceabc52805d05d281ce7c17722f966
yoshimasa.yokoyama <yoshimasa.yokoyama@i-studio.co.jp>
2015-12-06 18:12:39 +0900

    add goodbye to controller and change the root to #goodbye

--- yokoyama/hello_app/app/controllers/application_controller.rb
+++ yokoyama/hello_app/app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  #hello
   def hello
     render text: 'hello,world!'
+  end
+
+  #goodbye
+  def goodbye
+    render text: 'goodbye,world!'
   end
 end
